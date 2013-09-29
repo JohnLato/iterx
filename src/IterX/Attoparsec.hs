0ea7860f82eb81b500cb91ada5a6eead499e4025
John Lato <jwlato@tsurucapital.com>
2013-09-29 18:42:57 -0500

    IterX: add Status/cxtFailure
    These give better support for parsing

--- src/IterX/Attoparsec.hs
+++ src/IterX/Attoparsec.hs
@@ -13,10 +13,10 @@ import           Control.Exception (throw)
 import           Data.Attoparsec
 import           Data.ByteString (ByteString)
 
-attoToIterX :: Monad m => Parser a -> IterX m ByteString a
-attoToIterX p = IterX $ \i onF onS ->
-    let step (Done s r) = onS s r
-        step (Fail s cxt err) = onF s (unlines (cxt ++ [err]))
+attoToIterX :: Monad m => Parser a -> IterX ByteString m a
+attoToIterX p = IterX $ \i st onF onS ->
+    let step (Done s r) = onS s st r
+        step (Fail s cxt err) = onF s st (unlines (cxt ++ [err]))
         step (Partial k) = return . MoreX $ step . k
     in step (parse p i)
 
--- src/IterX/IterX.hs
+++ src/IterX/IterX.hs
@@ -8,11 +8,13 @@
 module IterX.IterX (
   ResultX(..)
 , IterX(..)
+, Status(..)
 , Failure
 , Success
 
 , failX
 , doneX
+, cxtFailure
 
 , feed
 
@@ -31,63 +33,79 @@ import           Data.Monoid
 
 ----------------------------------------------------------------
 
-data ResultX m s r =
+data ResultX s m r =
     DoneX r s
   | FailX s String
-  | MoreX (s -> m (ResultX m s r))
+  | MoreX (s -> m (ResultX s m r))
     deriving (Functor)
 
-newtype IterX m s a = IterX {
+data Status = EOF | HasMore
+    deriving (Eq, Show)
+
+newtype IterX s m a = IterX {
     runIter :: forall r.
                s
+               -> Status
                -> Failure s m r
                -> Success s m a r
-               -> m (ResultX m s r)}
+               -> m (ResultX s m r)}
 
-type Failure s m r   = s -> String -> m (ResultX m s r)
-type Success s m a r = s -> a -> m (ResultX m s r)
+type Failure s m r   = s -> Status -> String -> m (ResultX s m r)
+type Success s m a r = s -> Status -> a -> m (ResultX s m r)
 
-instance Functor (IterX m s) where
+instance Functor (IterX s m) where
     {-# INLINE fmap #-}
     fmap   = mapIter
 
-instance Applicative (IterX m s) where
+instance Applicative (IterX s m) where
     {-# INLINE pure #-}
     pure   = returnIter
     {-# INLINE (<*>) #-}
     (<*>)  = apIter
 
-instance Monad (IterX m s) where
+instance Monad (IterX s m) where
     {-# INLINE return #-}
     return = returnIter
     {-# INLINE (>>=) #-}
     (>>=)  = bindIter
     fail   = failIter
 
-failX :: Monad m => s -> String -> m (ResultX m s a)
-failX s err = return $ FailX s err
+instance MonadTrans (IterX s) where
+    {-# INLINE lift #-}
+    lift m = IterX $ \s st _ onD -> m >>= onD s st
 
-doneX :: Monad m => s -> a -> m (ResultX m s a)
-doneX s a   = return $ DoneX a s
+failX :: Monad m => s -> Status -> String -> m (ResultX s m a)
+failX s _ err = return $ FailX s err
 
-returnIter :: a -> IterX m s a
-returnIter a = IterX $ \s _ onD -> onD s a
+doneX :: Monad m => s -> Status -> a -> m (ResultX s m a)
+doneX s _ a   = return $ DoneX a s
+
+-- | transform a failure continuation by adding extra context
+cxtFailure :: String -> IterX s m a -> IterX s m a
+cxtFailure cxt i = IterX $ \s st onF onD -> runIter i s st (cxtF onF) onD
+  where
+    cxtF onF s st err = onF s st (cxt ++ ": " ++ err)
+{-# INLINE cxtFailure #-}
+
+returnIter :: a -> IterX s m a
+returnIter a = IterX $ \s st _ onD -> onD s st a
 {-# NOINLINE returnIter #-}
 
-bindIter :: IterX m s a -> (a -> IterX m s b) -> IterX m s b
-bindIter m f = IterX $ \s onF onD -> runIter m s onF
-    $ \s' a -> runIter (f a) s' onF onD
+bindIter :: IterX s m a -> (a -> IterX s m b) -> IterX s m b
+bindIter m f = IterX $ \s st onF onD -> runIter m s st onF
+    $ \s' st' a -> runIter (f a) s' st' onF onD
 {-# INLINE bindIter #-}
 
-failIter :: String -> IterX m s a
-failIter err = IterX $ \s onF _ -> onF s err
+failIter :: String -> IterX s m a
+failIter err = IterX $ \s st onF _ -> onF s st err
 {-# INLINE failIter #-}
 
-mapIter :: (a -> b) -> IterX m s a -> IterX m s b
-mapIter f m = IterX $ \s onF onD -> runIter m s onF $ \s' a -> onD s' (f a)
+mapIter :: (a -> b) -> IterX s m a -> IterX s m b
+mapIter f m = IterX $ \s st onF onD -> runIter m s st onF $
+     \s' st' a -> onD s' st' (f a)
 {-# INLINE mapIter #-}
 
-apIter :: IterX m s (a -> b) -> IterX m s a -> IterX m s b
+apIter :: IterX s m (a -> b) -> IterX s m a -> IterX s m b
 apIter f a = do
     f' <- f
     a' <- a
@@ -99,11 +117,11 @@ apIter f a = do
 -- run an iteratee with the given producer.  Extra input after the iteratee
 -- completes is discarded.
 runIterX :: (Monad m)
-         => Producer (StateT (ResultX m s a) m) s
-         -> IterX m s a
-         -> m (ResultX m s a)
+         => Producer (StateT (ResultX s m a) m) s
+         -> IterX s m a
+         -> m (ResultX s m a)
 runIterX gen i = do
-    let s0 = MoreX $ \s -> runIter i s failX doneX
+    let s0 = MoreX $ \s -> runIter i s HasMore failX doneX
     foldG f s0 gen
   where
     f (MoreX k) s   = k s
@@ -112,7 +130,7 @@ runIterX gen i = do
 
 ----------------------------------------------------------------
 
-feed :: (Monad m, Monoid s) => ResultX m s r -> s -> m (ResultX m s r)
+feed :: (Monad m, Monoid s) => ResultX s m r -> s -> m (ResultX s m r)
 feed (MoreX k)    s = k s
 feed (DoneX r s0) s = return $ DoneX r (s0<>s)
 feed f@FailX{}    _ = return f
@@ -121,11 +139,11 @@ feed f@FailX{}    _ = return f
 
 -- | Create a 'Transducer' from an 'IterX'
 convStream :: Monad m
-           => IterX m e1 e2
-           -> Transducer (GenT e2 (StateT (ResultX m e1 e2) m)) m e1 e2
+           => IterX e1 m e2
+           -> Transducer (GenT e2 (StateT (ResultX e1 m e2) m)) m e1 e2
 convStream i = streamGM (f id) i0
   where
-    i0 = MoreX $ \inp -> runIter i inp failX doneX
+    i0 = MoreX $ \inp -> runIter i inp HasMore failX doneX
     f acc (MoreX k) s =  k s >>= \case
         res@(MoreX _) -> return (res,acc [])
         -- TODO: probably makes sense to unroll this at least once
@@ -135,13 +153,13 @@ convStream i = streamGM (f id) i0
 
 -- | Create a 'Transducer' from an 'IterX' generating function
 unfoldConvStream :: Monad m
-                 => (st -> IterX m e1 (st,e2))
+                 => (st -> IterX e1 m (st,e2))
                  -> st
-                 -> Transducer (GenT e2 (StateT (ResultX m e1 (st,e2)) m)) m
+                 -> Transducer (GenT e2 (StateT (ResultX e1 m (st,e2)) m)) m
                       e1 e2
 unfoldConvStream mkI st0 = streamGM (f id) (i0 st0)
   where
-    i0 s = MoreX $ \inp -> runIter (mkI s) inp failX doneX
+    i0 s = MoreX $ \inp -> runIter (mkI s) inp HasMore failX doneX
     -- TODO: probably makes sense to unroll this at least once
     f acc (MoreX k) s   = k s >>= \case
         res@(MoreX _) -> return (res,acc [])
