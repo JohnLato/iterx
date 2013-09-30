62359258bbd948eeb4283c9f6bd22c641d0ff111
John Lato <jwlato@tsurucapital.com>
2013-09-30 02:30:35 -0500

    StreamTrans: add StreamTransM

--- src/IterX/StreamTrans.hs
+++ src/IterX/StreamTrans.hs
@@ -1,6 +1,8 @@
 module IterX.StreamTrans (
   StreamTrans(..)
+, StreamTransM(..)
 , transG
+, transGM
 ) where
 
 import IterX.Core
@@ -8,8 +10,15 @@ import Control.Monad.State
 
 newtype StreamTrans a b = StreamTrans { unST :: a -> (StreamTrans a b, b) }
 
+newtype StreamTransM m a b =
+    StreamTransM { unSTM :: a -> m (StreamTransM m a b, b) }
+
 transG :: Monad m
        => StreamTrans e1 [e2]
        -> Transducer (GenT e2 (StateT (StreamTrans e1 [e2]) m)) m e1 e2
 transG = streamG unST
 
+transGM :: Monad m
+        => StreamTransM m e1 [e2]
+        -> Transducer (GenT e2 (StateT (StreamTransM m e1 [e2]) m)) m e1 e2
+transGM = streamGM unSTM
